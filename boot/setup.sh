836b96b99d3fb1d84f28f72089b181b169e55dfa
Matt Smillie <matt.smillie@joyent.com>
2014-07-22 19:31:14 -0700

    HEAD-1990 Separate ZK and binder zones in SDC.

--- boot/setup.sh
+++ boot/setup.sh
@@ -13,47 +13,14 @@ if [[ -h $SOURCE ]]; then
 fi
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 SVC_ROOT=/opt/smartdc/binder
-BINDER_ROOT=/binder
-# ZONE_UUID=$(zonename)
-# ZONE_DATASET=zones/$ZONE_UUID/data
 
 export PATH=$SVC_ROOT/build/node/bin:/opt/local/bin:/usr/sbin/:/usr/bin:$PATH
 
-# Install zookeeper package, need to touch this file to disable the license prompt
-# touch /opt/local/.dli_license_accepted
-
-function manta_setup_zookeeper {
-    manta_add_logadm_entry "zookeeper" "/var/log"
-
-    svccfg import /opt/smartdc/binder/smf/manifests/zookeeper.xml || \
-        fatal "unable to import ZooKeeper"
-    svcadm enable zookeeper || fatal "unable to start ZooKeeper"
-}
-
+# ZK-related common setup.
 source /opt/smartdc/boot/zk_common.sh
-setup_zk_delegated_dataset ${BINDER_ROOT}
 
+zk_common_delegated_dataset
 
-#
-# Set up zookeeper db in the delegated dataset (if it exists)
-#
-# mkdir -p $BINDER_ROOT
-# zfs list $ZONE_DATASET && rc=$? || rc=$?
-# if [[ $rc == 0 ]]; then
-#     mountpoint=$(zfs get -H -o value mountpoint $ZONE_DATASET)
-#     if [[ $mountpoint != $BINDER_ROOT ]]; then
-#         zfs set mountpoint=$BINDER_ROOT $ZONE_DATASET || \
-#             fatal "failed to set mountpoint"
-#     fi
-# fi
-# # Zookeeper must own its directory.
-# chmod 777 $BINDER_ROOT
-# sudo -u zookeeper mkdir -p $BINDER_ROOT/zookeeper
-
-#
-# XXX in the future this should come from SAPI and we should be pulling out
-# the "application" that's the parent of this instance. (see: SAPI-173)
-#
 if [[ -n $(mdata-get sdc:tags.manta_role) ]]; then
     export FLAVOR="manta"
 else
@@ -75,14 +42,16 @@ if [[ ${FLAVOR} == "manta" ]]; then
     manta_common_setup "binder"
 
     echo "Setting up ZooKeeper"
-    manta_setup_zookeeper
+    # manta_setup_zookeeper
+    manta_add_logadm_entry "zookeeper" "/var/log"
 
-    manta_ensure_zk
+    zk_common_import ${SVC_ROOT}
 
     echo "Installing binder"
-    svccfg import $SVC_ROOT/smf/manifests/binder.xml || \
-        fatal "unable to import binder"
-    svcadm enable binder || fatal "unable to start binder"
+    svccfg import ${SVC_ROOT}/smf/manifests/binder.xml
+    # || \
+    #     fatal "unable to import binder"
+    # svcadm enable binder || fatal "unable to start binder"
 
     manta_common_setup_end
 
@@ -97,13 +66,13 @@ else # FLAVOR == "sdc"
     mkdir -p /var/smartdc/binder
     mkdir -p /opt/smartdc/etc
 
-    echo "Importing zookeeper SMF manifest."
-    [[ -z $(/usr/bin/svcs -a | grep zookeeper) ]] && \
-      /usr/sbin/svccfg import /opt/smartdc/binder/smf/manifests/zookeeper.xml
+    echo "Setting up zookeeper."
+    zk_common_import ${SVC_ROOT}
 
     echo "Importing binder SMF manifest."
-    [[ -z $(/usr/bin/svcs -a | grep binder) ]] && \
-      /usr/sbin/svccfg import /opt/smartdc/binder/smf/manifests/binder.xml
+    svccfg import /opt/smartdc/binder/smf/manifests/binder.xml \
+        || fatal "unable to import binder manifest"
+    svcadm enable binder || fatal "unable to start binder"
 
     # Log rotation.
     sdc_log_rotation_add amon-agent /var/svc/log/*amon-agent*.log 1g
