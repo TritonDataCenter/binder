fe8e803e33b08ae94a15f540345c9455540f4171
Nate Fitch <nate.fitch@joyent.com>
2014-08-25 16:13:58 +0000

    TOOLS-607: I for one, welcome my new open source license

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,15 @@
-// Copyright 2012 Mark Cavage.  All rights reserved.
-//
-// Just a simple wrapper over nodeunit's exports syntax. Also exposes
-// a common logger for all tests.
-//
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+// Just a simple wrapper over nodeunit's exports syntax. Also exposes a common
+// logger for all tests.
 
 var bunyan = require('bunyan');
 var Cache = require('expiring-lru-cache');
